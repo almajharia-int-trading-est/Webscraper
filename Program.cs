41168c8882b3d8e9ffcbaecd0a0bffe6d7642eaf
nabeelk1 <95003549+nabeelk1@users.noreply.github.com>
2025-12-08 11:10:51 +0300

    Minor changes.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var proxies = File.ReadAllLines("proxies.txt").Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+
+        foreach (var proxy in proxies)
+        {
+            Console.WriteLine($"Using proxy: {proxy}");
+
+            using var http = new HttpClient();
+            var requestBody = new
+            {
+                cmd = "request.get",
+                url = "https://www.thermofisher.com/order/catalog/product/E1041",
+                session = "my-session",
+                proxy = new
+                {
+                    url = $"{proxy}"
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(requestBody);
+            var response = await http.PostAsync("http://localhost:8191/v1",
+                new StringContent(json, Encoding.UTF8, "application/json"));
+
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
